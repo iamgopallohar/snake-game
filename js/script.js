3f236ffba2e7bfbc3f25dc7081b5fb736a6bd799
Gopal <iamgopallohar@gmail.com>
2024-01-30 01:54:53 +0530

    Moving Snake

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ class Snake {
       new Element({ type: "body", position: { x: 2, y: 2 }, board: gameBoard }),
       new Element({ type: "body", position: { x: 1, y: 2 }, board: gameBoard }),
     ];
+    this.direction = { x: 0, y: 1 };
   }
 }
 
@@ -28,4 +29,34 @@ class Element {
   }
 }
 
-new Snake(document.getElementById("gameboard"));
+const snake = new Snake(document.getElementById("gameboard"));
+
+let lastPaintTime;
+const SPEED = 1;
+function animation(currentTime) {
+  window.requestAnimationFrame(animation);
+  if (lastPaintTime == undefined) {
+    lastPaintTime = currentTime;
+    return;
+  }
+  if (currentTime - lastPaintTime < 1000 / SPEED) return;
+  lastPaintTime = currentTime;
+
+  for (
+    let elementIndex = snake.elements.length - 1;
+    elementIndex >= 0;
+    elementIndex--
+  ) {
+    let element = snake.elements[elementIndex];
+    if (elementIndex == 0) {
+      element.position = {
+        x: element.position.x + snake.direction.x,
+        y: element.position.y + snake.direction.y,
+      };
+    } else {
+      element.position = { ...snake.elements[elementIndex - 1].position };
+    }
+  }
+}
+
+window.requestAnimationFrame(animation);
