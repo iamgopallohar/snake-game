a62e77f60685bbcbfacfb8becee9149f453f6261
Gopal <iamgopallohar@gmail.com>
2024-01-30 02:48:00 +0530

    collision detection

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 class Snake {
   constructor(gameBoard) {
     this.elements = [
-      new Element({ type: "head", position: { x: 3, y: 2 }, board: gameBoard }),
+      new Element({
+        type: "head",
+        position: { x: 8, y: 2 },
+        board: gameBoard,
+      }),
+      // new Element({ type: "body", position: { x: 9, y: 2 }, board: gameBoard }),
+      // new Element({ type: "body", position: { x: 8, y: 2 }, board: gameBoard }),
+      new Element({ type: "body", position: { x: 7, y: 2 }, board: gameBoard }),
+      new Element({ type: "body", position: { x: 6, y: 2 }, board: gameBoard }),
+      new Element({ type: "body", position: { x: 5, y: 2 }, board: gameBoard }),
+      new Element({ type: "body", position: { x: 4, y: 2 }, board: gameBoard }),
+      new Element({ type: "body", position: { x: 3, y: 2 }, board: gameBoard }),
       new Element({ type: "body", position: { x: 2, y: 2 }, board: gameBoard }),
       new Element({ type: "body", position: { x: 1, y: 2 }, board: gameBoard }),
     ];
     this.direction = { x: 0, y: 1 };
   }
+
+  render = () => this.elements.forEach((element) => element.render());
 }
 
 class Element {
@@ -15,26 +28,24 @@ class Element {
     this.element.classList.add("snake-element");
     this.element.classList.add(type);
     this.position = { ...position };
+    this.render();
     board.append(this.element);
   }
 
-  get position() {
-    return this.positionObj;
-  }
-
-  set position(positionObj) {
-    this.positionObj = positionObj;
-    this.element.style.setProperty("--grid-column-start", positionObj.x);
-    this.element.style.setProperty("--grid-row-start", positionObj.y);
+  render() {
+    this.element.style.setProperty("--grid-column-start", this.position.x);
+    this.element.style.setProperty("--grid-row-start", this.position.y);
   }
 }
 
 const snake = new Snake(document.getElementById("gameboard"));
 
 let lastPaintTime;
-const SPEED = 12;
+let animationId;
+const GRID_SIZE = 30;
+const SPEED = 1;
 function animation(currentTime) {
-  window.requestAnimationFrame(animation);
+  animationId = window.requestAnimationFrame(animation);
   if (lastPaintTime == undefined) {
     lastPaintTime = currentTime;
     return;
@@ -42,30 +53,52 @@ function animation(currentTime) {
   if (currentTime - lastPaintTime < 1000 / SPEED) return;
   lastPaintTime = currentTime;
 
+  // update positions of elements before the collision detection
   for (
     let elementIndex = snake.elements.length - 1;
     elementIndex >= 0;
     elementIndex--
   ) {
     let element = snake.elements[elementIndex];
-    if (elementIndex == 0) {
+    if (elementIndex !== 0) {
+      element.position = { ...snake.elements[elementIndex - 1].position };
+    } else {
+      // head - it is updated at last so collision detection here
       element.position = {
         x: element.position.x + snake.direction.x,
         y: element.position.y + snake.direction.y,
       };
-    } else {
-      element.position = { ...snake.elements[elementIndex - 1].position };
     }
+  }
+
+  // collision detection before rendering so that we don't have that problematic effect
+  let collision = snake.elements.some((element, elementIndex) => {
+    if (elementIndex === 0) {
+      if (
+        element.position.x > GRID_SIZE ||
+        element.position.x < 0 ||
+        element.position.y > GRID_SIZE ||
+        element.position.y < 0
+      )
+        return true;
+    } else {
+      if (
+        element.position.x === snake.elements[0].position.x &&
+        element.position.y === snake.elements[0].position.y
+      )
+        return true;
+    }
+  });
+
+  if (collision === true) {
+    window.cancelAnimationFrame(animationId);
+  } else {
+    snake.render();
   }
 }
 
-window.requestAnimationFrame(animation);
+animationId = window.requestAnimationFrame(animation);
 document.addEventListener("keydown", (e) => {
-  console.log(e.key);
-  console.log(e.key === "ArrowDown");
-  console.log(e.key === "ArrowUp");
-  console.log(e.key === "ArrowLeft");
-  console.log(e.key === "ArrowRight");
   switch (e.key) {
     case "ArrowUp":
     case "w":
