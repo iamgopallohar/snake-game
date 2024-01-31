8fa85332ae39c0e1b5b3e369c0a4c6eec52a62d0
Gopal <iamgopallohar@gmail.com>
2024-01-31 18:59:39 +0530

    food and eating food implementation

--- js/Snake.js
+++ js/Snake.js
@@ -2,19 +2,60 @@ import Element from "./Element.js";
 
 export default class Snake {
   constructor({ gameBoard, GRID_SIZE }) {
-    this.elements = [
-      new Element({ type: "head", position: { x: 9, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 8, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 7, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 6, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 5, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 4, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 3, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 2, y: 2 }, board: gameBoard }),
-      new Element({ type: "body", position: { x: 1, y: 2 }, board: gameBoard }),
-    ];
-    this.direction = { x: 0, y: 1 };
+    this.gameBoard = gameBoard;
     this.GRID_SIZE = GRID_SIZE;
+    this.foodPower = 1;
+    this.elements = [
+      new Element({
+        type: "head",
+        position: { x: 9, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 8, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 7, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 6, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 5, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 4, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 3, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 2, y: 2 },
+        board: this.gameBoard,
+      }),
+      new Element({
+        type: "body",
+        position: { x: 1, y: 2 },
+        board: this.gameBoard,
+      }),
+    ];
+    this.food = [];
+    this.gameOver = false;
+    this.createFood();
+    this.direction = { x: 0, y: 1 };
   }
 
   get direction() {
@@ -51,29 +92,88 @@ export default class Snake {
     }
 
     // collision detection before rendering so that we don't have that problematic effect
-    let collision = this.elements.some((element, elementIndex) => {
-      if (elementIndex === 0) {
-        if (
-          element.position.x > this.GRID_SIZE ||
-          element.position.x < 1 ||
-          element.position.y > this.GRID_SIZE ||
-          element.position.y < 1
-        )
-          return true;
-      } else {
-        if (
-          element.position.x === this.elements[0].position.x &&
-          element.position.y === this.elements[0].position.y
-        )
-          return true;
-      }
-    });
+    this.detectCollision();
 
-    if (collision === true) {
+    if (this.gameOver === true) {
       window.cancelAnimationFrame(animationId);
     } else {
       this.render();
     }
+  }
+
+  detectCollision() {
+    this.gameOver = this.elements.some((element, elementIndex) => {
+      let wallCollision,
+        headCollision,
+        collidedFoodIndex = -1;
+      if (elementIndex === 0) {
+        // wall collision
+        wallCollision = this.checkCollisionWithWall(element);
+        // food collision
+        collidedFoodIndex = this.checkCollisionWithFood(element);
+      } else {
+        // collision into itself
+        headCollision = this.checkCollisionWithHead(element);
+      }
+      if (collidedFoodIndex !== -1) {
+        this.incrementLength(collidedFoodIndex);
+      }
+      return wallCollision || headCollision;
+    });
+  }
+
+  checkCollisionWithWall(head) {
+    return (
+      head.position.x > this.GRID_SIZE ||
+      head.position.x < 1 ||
+      head.position.y > this.GRID_SIZE ||
+      head.position.y < 1
+    );
+  }
+
+  checkCollisionWithFood(head) {
+    let collidedFoodIndex = -1;
+    this.food.some((food, foodIndex) => {
+      if (
+        food.position.x === head.position.x &&
+        food.position.y === head.position.y
+      ) {
+        collidedFoodIndex = foodIndex;
+        return true;
+      }
+    });
+    return collidedFoodIndex;
+  }
+
+  checkCollisionWithHead(element) {
+    return (
+      element.position.x === this.elements[0].position.x &&
+      element.position.y === this.elements[0].position.y
+    );
+  }
+
+  incrementLength(foodIndex) {
+    setTimeout(() => this.food.splice(foodIndex, 1)[0].element.remove());
+    let lastElementPosition = this.elements[this.elements.length - 1].position;
+    for (let index = 0; index < this.foodPower; index++) {
+      this.elements.push(
+        new Element({
+          type: "body",
+          position: { ...lastElementPosition },
+          board: this.gameBoard,
+        })
+      );
+    }
+  }
+
+  createFood() {
+    this.food.push(
+      new Element({
+        type: "food",
+        position: { x: 6, y: 20 },
+        board: this.gameBoard,
+      })
+    );
   }
 
   render = () => this.elements.forEach((element) => element.render());
